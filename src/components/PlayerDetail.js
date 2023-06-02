5d4c5c1cf7781b57f618328fc33aa5742a5ec437
Utkarsh Singh <utkarsh.singh@bonamisoftware.com>
2023-06-02 21:31:29 +0530

    More with detail page half done

--- src/components/PlayerDetail.js
+++ src/components/PlayerDetail.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import data from "../list.json";
 import { useNavigate, useParams } from "react-router-dom";
+import playerDetailStyles from '../css/PlayerDetail.module.css';
+
 
 function PlayerDetail() {
   const { playerid } = useParams();
@@ -11,30 +13,46 @@ function PlayerDetail() {
 
   useEffect(() => {
     const ply = data.find((player) => player.id == playerid);
+    const dcPly = JSON.parse(JSON.stringify(ply));
     if (ply) {
-      setPlayerDetail(ply);
-    }else{
-        setError(true);
+      setPlayerDetail(dcPly);
+    } else {
+      setError(true);
     }
   }, [playerid]);
 
   return (
     <>
-      
-      {!error && 
-      <div>
-        <div>PlayerDetail</div>
-        Name: {playerDetail.name}
-      </div>
-      }
-      {error && 
-      <div>
-        Something Went Wrong
-        <button onClick={() => {
-            navigate('/');
-        }} >Go To Home</button>
-      </div>
-      }
+      {!error && (
+        <div>
+          <div className={playerDetailStyles.title}>PlayerDetail</div>
+          <span><h6 className={playerDetailStyles.subtitle}>Name:</h6>{playerDetail.name}</span>
+          <br />
+          <div>
+            <span><h6 className={playerDetailStyles.subtitle}>Test Score:</h6>{playerDetail?.score?.Test}</span>
+          </div>
+          <div>
+            <span><h6 className={playerDetailStyles.subtitle}>ODI Score:</h6> {playerDetail?.score?.ODI}</span>
+          </div>
+          <div>
+            <span><h6 className={playerDetailStyles.subtitle}>T20I Score:</h6> {playerDetail?.score?.T20I}</span>
+          </div>
+          <br />
+          <span>Name: {playerDetail.name}</span>
+        </div>
+      )}
+      {error && (
+        <div>
+          Something Went Wrong
+          <button
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Go To Home
+          </button>
+        </div>
+      )}
     </>
   );
 }
